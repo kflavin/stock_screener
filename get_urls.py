5672124609e4208cac8ad0e6f4c0ab84f1fd5fa0
Kyle <Kyle.Flavin@gmail.com>
2015-09-21 22:37:23 -0700

    Added sorting for the stock dictionaries, and changed some of the filter code.

--- get_urls.py
+++ get_urls.py
@@ -9,21 +9,28 @@ from collections import namedtuple, OrderedDict
 
 results_file = "./stocks_screened.csv"
 
-# key is the label, value is the search string
-abbr = OrderedDict()
-abbr['ROE (%)'] = "Return on Equity"
-abbr['Free Cash Flow'] = "Levered Free Cash Flow"
-abbr['Profit Margin (%)'] = "Profit Margin"
-abbr['Operating Margin (%)'] = "Operating Margin"
-abbr['Total Debt/Equity'] = "Total Debt/Equity"
-abbr['P/E (ttm)'] = "Trailing P/E"
-abbr['P/E (forward)'] = "Forward P/E"
-abbr['Current Ratio'] = "Current Ratio"
-abbr['PEG'] = "PEG Ratio"
+# Filters
+filters = {'ROE (%)': ">19", 'Profit Margin (%)': '>9', 'Operating Margin (%)': '>9', 'Total Debt/Equity': '<100'}
 
+# Values to fetch from finance page
+# key is the label, value is the search string
+stats = OrderedDict()
+stats['ROE (%)'] = {"search": "Return on Equity", "filter": ">15"}
+stats['Free Cash Flow'] = {"search": "Levered Free Cash Flow"}
+stats['Profit Margin (%)'] = {"search": "Profit Margin", "filter": ">10"}
+stats['Operating Margin (%)'] = {"search": "Operating Margin", "filter": ">10"}
+stats['Total Debt/Equity'] = {"search": "Total Debt/Equity"}
+stats['P/E (ttm)'] = {"search": "Trailing P/E"}
+stats['P/E (forward)'] = {"search": "Forward P/E"}
+stats['Current Ratio'] = {"search": "Current Ratio"}
+stats['PEG'] = {"search": "PEG Ratio"}
+
+# Stock values is a dictionary of named tuples
 #stock_values = {}
 stock_values = OrderedDict()
 Stock = namedtuple('Stock', 'symbol sector subsector')
+
+stock_picks = OrderedDict()
 
 url = "http://finance.yahoo.com/q/ks?s=%s+Key+Statistics"
 bs_url = "http://finance.yahoo.com/q/bs?s=%s+Balance+Sheet&annual"
@@ -60,7 +67,7 @@ def get_field(line, field, delimiter=","):
 
 def get_stock_list(filename):
     """
-    Return a list of stocks, either from CSV file or text file.
+    Return a list of stocks to be operated on, either from CSV file or text file.
     """
     f = open(filename, "r")
     stocks = f.read().split("\n")
@@ -87,7 +94,7 @@ def get_stock_list(filename):
 
 def build_values(key_stats, industry_details, stock):
     """
-    Take values and build a dictionary for the given stock
+    Take values given from HTTP queries and build a dictionary for the given stock
     """
     
     stock_values = OrderedDict()
@@ -102,29 +109,32 @@ def build_values(key_stats, industry_details, stock):
             value = td.findNextSibling().text
             stock_values["Industry"] = value.strip("%")
 
-    if "Industry" not in stock_values.keys():
-        stock_values["Industry"] = ""
-    if "Sector" not in stock_values.keys():
-        stock_values["Sector"] = ""
+
+    stock_keys = stock_values.keys()
+
+    if "Industry" not in stock_keys:
+        stock_values["Industry"] = "-"
+    if "Sector" not in stock_keys:
+        stock_values["Sector"] = "-"
 
     # Get the values we need from the key statistics page
     soup = bs(key_stats, "html.parser")
 
 
-    for k,v in abbr.items():
+    # Find all matches from the data
+    for k,v in stats.items():
+        search = v['search']
         for td in soup.findAll("td"):
-            if td.text.startswith(v):
+            if td.text.startswith(search):
                 value = td.findNextSibling().text
                 stock_values[k] = value.strip("%")
 
-    #for td in soup.findAll("td"):
-    #    for k,v in abbr.items():
-    #        if td.text.startswith(v):
-    #            value = td.findNextSibling().text
-    #            stock_values[k] = value.strip("%")
+    # If the data is missing some matches, put in placeholders
+    for k,v in stats.items():
+        if k not in stock_keys:
+            stock_values[k] = "-"
 
     return stock_values
-
 
 @asyncio.coroutine
 def do_work(stock):
@@ -133,20 +143,29 @@ def do_work(stock):
     """
     global stock_values, sem
     with (yield from sem):
-        print("grabbed sem", sem, stock.symbol)
-        key_response = yield from aiohttp.request('GET', url % stock.symbol)
-        industry_response = yield from aiohttp.request('GET', id_url % stock.symbol)
-        #income_response = yield from aiohttp.request('GET', is_url % stock.symbol)
-        #balance_response = yield from aiohttp.request('GET', bs_url % stock.symbol)
-        #cash_flow_response = yield from aiohttp.request('GET', cf_url % stock.symbol)
+        #print("grabbed sem", sem, stock.symbol)
+        try:
+            key_response = yield from aiohttp.request('GET', url % stock.symbol)
+            industry_response = yield from aiohttp.request('GET', id_url % stock.symbol)
+            key_stats = yield from key_response.read()
+            industry_details = yield from industry_response.read()
+        except:
+            print("Failed to retrieve %s" % str(stock))
+            key_stats = ""
+            industry_details = ""
 
-    key_stats = yield from key_response.read()
-    industry_details = yield from industry_response.read()
-    #income = yield from income_response.read()
-    #balance = yield from balance_response.read()
-    #cash_flow = yield from cash_flow_response.read()
-    #stock_values[stock.symbol] = build_values(key_stats, income, balance, cash_flow, stock)
     stock_values[stock.symbol] = build_values(key_stats, industry_details, stock)
+
+def compare_values(val1, op, val2):
+    try:
+        v1 = float(val1)
+        v2 = float(val2)
+    except:
+        return False
+
+    expr = ("%s %s %s" % (v1, op, v2))
+    return eval(expr)
+
 
 if __name__ == '__main__':
 
@@ -159,16 +178,71 @@ if __name__ == '__main__':
     # stocks is a list of namedtuples
     stocks = get_stock_list(filename)
 
+    # Pull down all the data we'll need via HTTP
     loop = asyncio.get_event_loop()
     f = asyncio.wait([do_work(stock) for stock in stocks])
     loop.run_until_complete(f)
-    #print("your stocks", stocks)
-    #print("your values", stock_values)
 
+    # Sort the stocks
+    stock_values = sorted(stock_values.items(), key=lambda x: (x[1]['Sector'], x[1]['Industry'], x[1]['ROE (%)'], x[1]['Profit Margin (%)']))
+
+
+    # Apply filtering.  Keep the stocks we want in stock_picks
+    for stock,values in stock_values:
+        # iterate over all filters and check if the stock's value is acceptable
+        for label in values:
+            if label in filters.keys():
+                actual = values[label]
+                op = filters[label][0]
+                compare = filters[label][1:]
+                if not compare_values(actual, op, compare):
+                    keep = False
+
+        # If the stock didn't have the label we were looking for, exclude it
+        for label in filters.keys():
+            if label not in values:
+                found_labels = False
+
+        if keep and found_labels:
+            stock_picks[stock] = values
+
+        # Each iteration assumes we're keeping the stock, until we filter it out.
+        keep = True
+        found_labels = True
+
+
+    ## Apply filtering.  Keep the stocks we want in stock_picks
+    #keep = True
+    #found_labels = True
+    #for key,value in stock_values.items():
+    #    for label in stock_values[key]:
+    #        if label in filters.keys():
+    #            # Get values to compare
+    #            actual = stock_values[key][label]
+    #            op = filters[label][0]
+    #            compare = filters[label][1:]
+    #            if not compare_values(actual, op, compare):
+    #                keep = False
+
+    #    for label in filters.keys():
+    #        if label not in stock_values[key]:
+    #            found_labels = False
+
+    #    if keep and found_labels:
+    #        stock_picks[key] = value
+    #    keep = True
+    #    found_labels = True
+
+    # stock_values = entire S&P500
+    # stock_picks = filtered stocks
+    # I'm just blowing away stock_values for now.
+    stock_values = stock_picks
+
+    # Write out values to CSV file
     with open(results_file, 'w') as csvfile:
         #writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        # get header vlaues
+        # get header values
         print(stock_values[list(stock_values.keys())[0]])
         headers = list(stock_values[list(stock_values.keys())[0]].keys())
         headers.insert(0, "Symbol")
