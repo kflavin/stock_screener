c7b019e31f6ed635f8e90933c93d59767bcfb40f
Kyle <Kyle.Flavin@gmail.com>
2015-10-12 23:04:51 -0700

    Adding subcommands to the runner.

--- runner-stocks.py
+++ runner-stocks.py
@@ -3,11 +3,9 @@ import logging
 import click
 
 
-
-@click.command()
-@click.option('--input', default="sp1.csv", help='The filename of the input file')
+@click.group()
 @click.option('--debug', is_flag=True, default=False)
-def start(input, debug):
+def start(debug):
     if debug:
         debugLevel="DEBUG"
     else:
@@ -19,7 +17,18 @@ def start(input, debug):
     #fmt = logging.Formatter('%(asctime)s %(levelname)s %(filename)s - %(message)s')
     #sh.setFormatter(fmt)
     logger.addHandler(sh)
-    main(input)
+
+@click.command()
+@click.option('--infile', '-i', default="sp1.csv", help='The filename of the input file')
+def fetch(infile):
+    main(infile)
+
+@click.command()
+def populate():
+    print("hello, world!")
+
+start.add_command(fetch)
+start.add_command(populate)
 
 if __name__ == '__main__':
     start()
