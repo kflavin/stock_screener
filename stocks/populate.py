98db7ebe0b672f5b6cee31c433c3856f48fc1270
Kyle <Kyle.Flavin@gmail.com>
2015-10-18 23:53:37 -0700

    Generalizing the populator to import the required module at run time.

--- stocks/populate.py
+++ stocks/populate.py
@@ -13,14 +13,13 @@ def populate_index(index, outfile=None):
         outfile = "%s.new.csv" % index
 
     populator_name = "populator_%s" % index
-    mod_details = imp.find_module(populator_name, ["stocks/populators"])
-    module = imp.load_module(populator_name, *mod_details)
 
-    if index == "sp500":
-        populator = module.IndexPopulator(outfile)
-    else:
-        populator = module.IndexPopulator(outfile)
-        logger.error("Do not recognize index '%s'" % index)
+    try:
+        mod_details = imp.find_module(populator_name, ["stocks/populators"])
+        module = imp.load_module(populator_name, *mod_details)
+    except ImportError as e:
+        logger.error("Could not import module '%s'" % populator_name)
         return None
 
+    populator = module.IndexPopulator(outfile)
     populator.run()
