1654112739e6b631910d9a3c5fcadb2ba3d29881
Kyle <Kyle.Flavin@gmail.com>
2015-11-02 23:56:18 -0800

    Messing with database code.  Nothing functional yet.

--- stocks/db/__init__.py
+++ stocks/db/__init__.py
@@ -1,26 +1,104 @@
+import os
 
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Float, Boolean, Date
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy import Sequence
 
-Base = declarative_base()
-engine = create_engine('sqlite:///stocks.db', echo=True)
+print("Inside database creation")
 
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+dbfile = "stocks.db"
+
+Base = declarative_base()
+engine = create_engine('sqlite:///stocks.db')
+
+
+class Company(Base):
+    __tablename__ = 'company'
+    id = Column(Integer, Sequence('company_id_seq'), primary_key=True)
+    symbol = Column(String(50))
     name = Column(String(50))
-    fullname = Column(String(50))
-    password = Column(String(12))
+    sector = Column(String(50))
+    industry = Column(String(50))
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (
-        self.name, self.fullname, self.password)
+        return "<Company(name='%s', sector='%s', industry='%s')>" % (
+        self.name, self.sector, self.industry)
+
+class Indicators(Base):
+    __tablename__ = 'indicators'
+    id = Column(Integer, Sequence('indicator_id_seq'), primary_key=True)
+    date = Column(Date)     # Date
+    buy = Column(Boolean)   # Is it a buy?
+    roe = Column(Float)     # Return on Equity
+    fcf = Column(Float)     # Free Cash Flow
+    pm = Column(Float)      # Profit Margin(%)
+    om = Column(Float)      # Operating Margin (%)
+    tde = Column(Float)     # Total Debt/Equity (%)
+    pe = Column(Float)      # Price / Earnings
+    cr = Column(Float)      # Current Ratio
+    peg = Column(Float)     # PEG
+    eps = Column(Float)     # Diluted EPS
+    div = Column(Float)     # Dividend
+    pfy = Column(Float)     # Growth Past 5 Years (%)
+    nfy = Column(Float)     # Growth Next 5 Years (%)
+    cp = Column(Float)      # Current Price
+    fp = Column(Float)      # Future Price
+    b15 = Column(Float)     # 15% Buy
+    b12 = Column(Float)     # 12% Buy
+    company_id = Column(Integer, ForeignKey('company.id'))
+    company = relationship("Company", backref=backref('indicators',
+        order_by=id))
+
+    def __repr__(self):
+        return "<Indicators(date='%s', buy='%s')>" % (self.date, self.buy)
+
+def populate_indicators(stock_values):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+    for symbol,vals in stock_values.items():
+        company = session.query(Company).filter_by(symbol=symbol).one()
+
+        if not company:
+            company = Company(symbol=symbol)
+            session.add(company)
+            session.commit()
+
+        indicator = Indicators(date=datetime.today())
+        if k.startswith("Checked"):
+        elif k.startswith("ROE"):
+        elif k.startswith("Free Cash Flow"):
+
+#Checked
+#Sector
+#Industry
+#ROE (%)
+#Free Cash Flow
+#Profit Margin (%)
+#Operating Margin (%)
+#Total Debt/Equity
+#P/E (ttm)
+#P/E (forward)
+#Current Ratio
+#PEG
+#Diluted EPS
+#Dividend
+#Past 5 Years (%)
+#Next 5 Years (%)
+#Curr Price
+#Future Price
+#15% Buy
+#12% Buy
+
 
 Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+
+
+
+
+
+
 
