0a7c9d6d8c6532d60d5c035e054a4366de5c0cca
Kyle <Kyle.Flavin@gmail.com>
2015-11-03 22:53:04 -0800

    Semi-working database module.  It needs to be able to handle all indicators.  Also need to write in some tests and better debugging code.

--- stocks/db/__init__.py
+++ stocks/db/__init__.py
@@ -1,17 +1,22 @@
 import os
+import logging
+from datetime import datetime
 
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, Boolean, Date
 from sqlalchemy.orm import sessionmaker, relationship, backref
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy import Sequence
 
 print("Inside database creation")
 
+logger = logging.getLogger(__name__)
+
 dbfile = "stocks.db"
 
 Base = declarative_base()
-engine = create_engine('sqlite:///stocks.db')
+engine = create_engine('sqlite:///stocks.db', echo=False)
 
 
 class Company(Base):
@@ -23,8 +28,8 @@ class Company(Base):
     industry = Column(String(50))
 
     def __repr__(self):
-        return "<Company(name='%s', sector='%s', industry='%s')>" % (
-        self.name, self.sector, self.industry)
+        return "<Company(symbol='%s', name='%s', sector='%s', industry='%s')>" % (
+        self.symbol, self.name, self.sector, self.industry)
 
 class Indicators(Base):
     __tablename__ = 'indicators'
@@ -32,45 +37,72 @@ class Indicators(Base):
     date = Column(Date)     # Date
     buy = Column(Boolean)   # Is it a buy?
     roe = Column(Float)     # Return on Equity
-    fcf = Column(Float)     # Free Cash Flow
-    pm = Column(Float)      # Profit Margin(%)
-    om = Column(Float)      # Operating Margin (%)
-    tde = Column(Float)     # Total Debt/Equity (%)
-    pe = Column(Float)      # Price / Earnings
-    cr = Column(Float)      # Current Ratio
-    peg = Column(Float)     # PEG
-    eps = Column(Float)     # Diluted EPS
-    div = Column(Float)     # Dividend
-    pfy = Column(Float)     # Growth Past 5 Years (%)
-    nfy = Column(Float)     # Growth Next 5 Years (%)
-    cp = Column(Float)      # Current Price
-    fp = Column(Float)      # Future Price
-    b15 = Column(Float)     # 15% Buy
-    b12 = Column(Float)     # 12% Buy
+    fcf = Column(String)     # Free Cash Flow
+    #pm = Column(Float)      # Profit Margin(%)
+    #om = Column(Float)      # Operating Margin (%)
+    #tde = Column(Float)     # Total Debt/Equity (%)
+    #pe = Column(Float)      # Price / Earnings
+    #cr = Column(Float)      # Current Ratio
+    #peg = Column(Float)     # PEG
+    #eps = Column(Float)     # Diluted EPS
+    #div = Column(Float)     # Dividend
+    #pfy = Column(Float)     # Growth Past 5 Years (%)
+    #nfy = Column(Float)     # Growth Next 5 Years (%)
+    #cp = Column(Float)      # Current Price
+    #fp = Column(Float)      # Future Price
+    #b15 = Column(Float)     # 15% Buy
+    #b12 = Column(Float)     # 12% Buy
     company_id = Column(Integer, ForeignKey('company.id'))
     company = relationship("Company", backref=backref('indicators',
         order_by=id))
 
     def __repr__(self):
-        return "<Indicators(date='%s', buy='%s')>" % (self.date, self.buy)
+        return "<Indicators(date='%s', buy='%s', roe='%s', fcf='%s')>" % (self.date, 
+                self.buy, self.roe, self.fcf)
 
 def populate_indicators(stock_values):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-
     for symbol,vals in stock_values.items():
-        company = session.query(Company).filter_by(symbol=symbol).one()
-
-        if not company:
+        try:
+            company = session.query(Company).filter_by(symbol=symbol).one()
+        except NoResultFound as e:
             company = Company(symbol=symbol)
             session.add(company)
-            session.commit()
+        else:
+            if not company:
+                company = Company(symbol=symbol)
+                session.add(company)
 
-        indicator = Indicators(date=datetime.today())
-        if k.startswith("Checked"):
-        elif k.startswith("ROE"):
-        elif k.startswith("Free Cash Flow"):
+        query = session.query(Company).filter_by(symbol=symbol)
+        count = query.filter(Company.indicators.any(Indicators.date ==
+            datetime.today().date())).count()
+
+        if count >= 1:
+            # We already have today's numbers
+            logger.info("Skipping %s" % symbol)
+            next;
+        else:
+            logger.info("Count for %s is %s" % (symbol, count))
+
+        inds = {}
+        for ind,val in vals.items():
+            if ind.startswith("Checked"):
+                if val:
+                    inds['buy'] = True
+                else:
+                    inds['buy'] = False
+            elif ind.startswith("ROE"):
+                inds['roe'] = val
+            elif ind.startswith("Free Cash Flow"):
+                inds['fcf'] = val
+
+        indicator = Indicators(date=datetime.today(), **inds)
+        company.indicators.append(indicator)
+        session.add(company)
+        session.commit()
+
 
 #Checked
 #Sector
