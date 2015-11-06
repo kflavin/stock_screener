d6be7caf02ce267bb49c728ed354fdd02278c456
Kyle <Kyle.Flavin@gmail.com>
2015-11-05 23:44:46 -0800

    Expanding stock indicators, fixing up database tests, getting closer to PEP8 compliant code.

--- stocks/db/__init__.py
+++ stocks/db/__init__.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from datetime import datetime
 
@@ -9,8 +8,6 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy import Sequence
 
-print("Inside database creation")
-
 logger = logging.getLogger(__name__)
 
 dbfile = "stocks.db"
@@ -20,6 +17,9 @@ engine = create_engine('sqlite:///stocks.db', echo=False)
 
 
 class Company(Base):
+    """
+    Model for storing companies.
+    """
     __tablename__ = 'company'
     id = Column(Integer, Sequence('company_id_seq'), primary_key=True)
     symbol = Column(String(50))
@@ -28,46 +28,94 @@ class Company(Base):
     industry = Column(String(50))
 
     def __repr__(self):
-        return "<Company(symbol='%s', name='%s', sector='%s', industry='%s')>" % (
-        self.symbol, self.name, self.sector, self.industry)
+        return ("< Company(symbol='%s',"
+                " name='%s',"
+                " sector='%s',"
+                " industry='%s,'"
+                ")>"
+                ) % (self.symbol,
+                     self.name,
+                     self.sector,
+                     self.industry,
+                     )
+
 
 class Indicators(Base):
+    """
+    Model for storing stock indicators.
+    Definitions are commented below.
+    """
     __tablename__ = 'indicators'
     id = Column(Integer, Sequence('indicator_id_seq'), primary_key=True)
     date = Column(Date)     # Date
     buy = Column(Boolean)   # Is it a buy?
     roe = Column(Float)     # Return on Equity
-    fcf = Column(String)     # Free Cash Flow
-    #pm = Column(Float)      # Profit Margin(%)
-    #om = Column(Float)      # Operating Margin (%)
-    #tde = Column(Float)     # Total Debt/Equity (%)
-    #pe = Column(Float)      # Price / Earnings
-    #cr = Column(Float)      # Current Ratio
-    #peg = Column(Float)     # PEG
-    #eps = Column(Float)     # Diluted EPS
-    #div = Column(Float)     # Dividend
-    #pfy = Column(Float)     # Growth Past 5 Years (%)
-    #nfy = Column(Float)     # Growth Next 5 Years (%)
-    #cp = Column(Float)      # Current Price
-    #fp = Column(Float)      # Future Price
-    #b15 = Column(Float)     # 15% Buy
-    #b12 = Column(Float)     # 12% Buy
+    fcf = Column(String)    # Free Cash Flow (may contain 'M' or 'B')
+    pm = Column(Float)      # Profit Margin(%)
+    om = Column(Float)      # Operating Margin (%)
+    tde = Column(Float)     # Total Debt/Equity (%)
+    pe = Column(Float)      # Price / Earnings
+    cr = Column(Float)      # Current Ratio
+    peg = Column(Float)     # PEG
+    eps = Column(Float)     # Diluted EPS
+    div = Column(Float)     # Dividend
+    pfy = Column(Float)     # Growth Past 5 Years (%)
+    nfy = Column(Float)     # Growth Next 5 Years (%)
+    cp = Column(Float)      # Current Price
+    fp = Column(Float)      # Future Price
+    b15 = Column(Float)     # 15% Buy
+    b12 = Column(Float)     # 12% Buy
     company_id = Column(Integer, ForeignKey('company.id'))
-    company = relationship("Company", backref=backref('indicators',
-        order_by=id))
+    company = relationship("Company",
+                           backref=backref('indicators', order_by=id))
 
     def __repr__(self):
-        return "<Indicators(date='%s', buy='%s', roe='%s', fcf='%s')>" % (self.date, 
-                self.buy, self.roe, self.fcf)
+        return ("<Indicators("
+                " company='%s',"
+                " date='%s',"
+                " buy='%s',"
+                " roe='%s',"
+                " fcf='%s',"
+                " pm='%s',"
+                " b15='%s',"
+                " b12='%s',"
+                ")>" % (self.company.symbol,
+                        self.date,
+                        self.buy,
+                        self.roe,
+                        self.fcf,
+                        self.pm,
+                        self.b15,
+                        self.b12,
+                        )
+                )
+
+
+def cast_float(value):
+    """
+    Handle bad data.
+    """
+    try:
+        ret = float(value)
+        return ret
+    except ValueError:
+        return 0.0
+
 
 def populate_indicators(stock_values):
+    """
+    Populate the associated indicators for each stock.  There should be only
+    one indicator per stock, per day.  This is hardcoded to match the values we
+    pull in, and then translated to our own internal representation of each
+    indicator.
+    """
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for symbol,vals in stock_values.items():
+    for symbol, vals in stock_values.items():
         try:
             company = session.query(Company).filter_by(symbol=symbol).one()
-        except NoResultFound as e:
+        except NoResultFound:
             company = Company(symbol=symbol)
             session.add(company)
         else:
@@ -77,60 +125,58 @@ def populate_indicators(stock_values):
 
         query = session.query(Company).filter_by(symbol=symbol)
         count = query.filter(Company.indicators.any(Indicators.date ==
-            datetime.today().date())).count()
+                             datetime.today().date())).count()
 
         if count >= 1:
             # We already have today's numbers
             logger.info("Skipping %s" % symbol)
-            next;
+            continue
         else:
             logger.info("Count for %s is %s" % (symbol, count))
 
         inds = {}
-        for ind,val in vals.items():
+        for ind, val in vals.items():
             if ind.startswith("Checked"):
                 if val:
                     inds['buy'] = True
                 else:
                     inds['buy'] = False
             elif ind.startswith("ROE"):
-                inds['roe'] = val
+                inds['roe'] = cast_float(val)
             elif ind.startswith("Free Cash Flow"):
                 inds['fcf'] = val
+            elif ind.startswith("Profit Margin"):
+                inds['pm'] = cast_float(val)
+            elif ind.startswith("Operating Margin"):
+                inds['om'] = cast_float(val)
+            elif ind.startswith("Total Debt/Equity"):
+                inds['tde'] = cast_float(val)
+            elif ind.startswith("P/E"):
+                inds['pe'] = cast_float(val)
+            elif ind.startswith("cr"):
+                inds['cr'] = cast_float(val)
+            elif ind.startswith("PEG"):
+                inds['peg'] = cast_float(val)
+            elif ind.startswith("Diluted EPS"):
+                inds['eps'] = cast_float(val)
+            elif ind.startswith("Dividend"):
+                inds['div'] = cast_float(val)
+            elif ind.startswith("Past 5 Years"):
+                inds['pfy'] = cast_float(val)
+            elif ind.startswith("Next 5 Years"):
+                inds['nfy'] = cast_float(val)
+            elif ind.startswith("Curr Price"):
+                inds['cp'] = cast_float(val)
+            elif ind.startswith("Future Price"):
+                inds['fp'] = cast_float(val)
+            elif ind.startswith("15% Buy"):
+                inds['b15'] = cast_float(val)
+            elif ind.startswith("12% Buy"):
+                inds['b12'] = cast_float(val)
 
         indicator = Indicators(date=datetime.today(), **inds)
         company.indicators.append(indicator)
         session.add(company)
         session.commit()
 
-
-#Checked
-#Sector
-#Industry
-#ROE (%)
-#Free Cash Flow
-#Profit Margin (%)
-#Operating Margin (%)
-#Total Debt/Equity
-#P/E (ttm)
-#P/E (forward)
-#Current Ratio
-#PEG
-#Diluted EPS
-#Dividend
-#Past 5 Years (%)
-#Next 5 Years (%)
-#Curr Price
-#Future Price
-#15% Buy
-#12% Buy
-
-
 Base.metadata.create_all(engine)
-
-
-
-
-
-
-
--- tests/test_stocks_db.py
+++ tests/test_stocks_db.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 from stocks.db import Indicators, Company, Base
 
@@ -9,7 +8,6 @@ from stocks.db import Indicators, Company, Base
 class TestDatabase(object):
     def setup(self):
         # print("running setup")
-        # self.base = declarative_base()
         self.engine = create_engine("sqlite:///:memory:")
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
@@ -28,7 +26,15 @@ class TestDatabase(object):
 
     def add_one_indicator(self, **kwargs):
         indicator = Indicators(date=kwargs['date'], buy=kwargs['buy'],
-                                fcf=kwargs['fcf'], roe=kwargs['roe'])
+                               fcf=kwargs['fcf'], roe=kwargs['roe'],
+                               pm=kwargs['pm'], om=kwargs['om'],
+                               tde=kwargs['tde'], pe=kwargs['pe'],
+                               cr=kwargs['cr'], peg=kwargs['peg'],
+                               eps=kwargs['eps'], div=kwargs['div'],
+                               pfy=kwargs['pfy'], nfy=kwargs['nfy'],
+                               cp=kwargs['cp'], fp=kwargs['fp'],
+                               b15=kwargs['b15'], b12=kwargs['b12']
+                               )
         self.session.add(indicator)
         self.session.commit()
         return indicator
@@ -37,7 +43,8 @@ class TestDatabase(object):
         return self.session.query(Company).filter_by(symbol=symbol).all()
 
     def query_for_indicator(self, symbol, date):
-        indicator = self.session.query(Indicators).filter_by(date = date).filter(Company.symbol == symbol).all()
+        indicator = self.session.query(Indicators).filter_by(date=date).\
+            filter(Company.symbol == symbol).all()
         return indicator
 
     def test_create_stock(self):
@@ -47,11 +54,31 @@ class TestDatabase(object):
         assert query[0].name == "Apple"
 
     def test_create_indicator(self):
-        company = self.add_one_stock("Apple", "AAPL", "Technology", "Technology")
+        company = self.add_one_stock("Apple",
+                                     "AAPL",
+                                     "Technology",
+                                     "Technology")
         date = datetime.today().date()
-        indicator = self.add_one_indicator(date=date, buy=True, roe=10.0, fcf=15.0)
+        indicator = self.add_one_indicator(date=date,
+                                           buy=True,
+                                           roe=10.0,
+                                           fcf="150M",
+                                           pm=15.0,
+                                           om=15.0,
+                                           tde=15.0,
+                                           pe=15.0,
+                                           cr=15.0,
+                                           peg=15.0,
+                                           eps=15.0,
+                                           div=15.0,
+                                           pfy=15.0,
+                                           nfy=15.0,
+                                           cp=15.0,
+                                           fp=15.0,
+                                           b15=15.0,
+                                           b12=15.0,
+                                           )
         company.indicators.append(indicator)
 
         query = self.query_for_indicator("AAPL", date)
         assert len(query) == 1
-
